d511904733c99fa4db56f043abfb73a43dd4caea
Daniel Estévez <daniel@destevez.net>
2016-08-26 18:28:11 +0200

    WAV 48kHz streamers

--- README.md
+++ README.md
@@ -6,10 +6,41 @@ different SDR hardware, recordings and other sources to UDP. The UDP data can be
 processed by another program.
 
 The stream format is 1 channel, 48kHz, little-endian int16_t. It is the same
-format that gqrx uses to stream audio
-(http://gqrx.dk/doc/streaming-audio-over-udp)
+format that [gqrx uses to stream audio](http://gqrx.dk/doc/streaming-audio-over-udp)
 
-The main goal of this repository is to be used with gr-satellites
-(https://github.com/daniestevez/gr-satellites), but it can be used with many
-other pieces of software.
+The main goal of this repository is to be used with
+[gr-satellites](https://github.com/daniestevez/gr-satellites), but it can be
+used with many other pieces of software.
 
+## Usage
+
+You can open the `.grc` file with `gnuradio-companion` and edit the parameters
+(they are on the upper part of the flowgraph). You can also run the `.py` script
+and specify the parameters on the command line. Use the `-h` flag to get help
+on how to specify the parameters.
+
+## WAV file streamers
+
+The WAV file streamers can be:
+
+  * Fast. This means that the WAV is streammed as fast as possible. The
+    receiving application may loose data if it can't process it as fast. The
+    streamer should run almost instantly.
+  * Realtime. This means that the WAV file is played in realtime at normal
+    speed. The streamer runs for the whole duration of the WAV file at normal
+    playback speed.
+
+The WAV file streamers also depend on the endiannes of your platform (if in
+doubt, try little-endian first).
+
+The available streamers are the following:
+
+  * `wav_48kHz_fast_be` 48kHz WAV fast streamer (big-endian)
+  * `wav_48kHz_fast_le` 48kHz WAV fast streamer (little-endian)
+  * `wav_48kHz_realtime_be` 48kHz WAV realtime streamer (big-endian)
+  * `wav_48kHz_realtime_le` 48kHz WAV realtime streamer (little-endian)
+
+*Hint:* It is sometimes useful to play a WAV file repeatedly in loop. By default
+ the streamers will only play the file once, but you can edit them with
+ `gnuradio-companion` and set the WAV file to repeat.
+ 
---
+++ wav_48kHz_fast_be.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2
+##################################################
+# GNU Radio Python Flow Graph
+# Title: WAV 48KHz file fast streamer (big-endian platform)
+# Author: Daniel Estevez
+# Description: Streams a WAV 48kHz file fast (i.e., as fast as possible, not in real time)
+# Generated: Fri Aug 26 18:18:03 2016
+##################################################
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+
+class wav_48kHz_fast_be(gr.top_block):
+
+    def __init__(self, input_file="", port=7355, destination="localhost"):
+        gr.top_block.__init__(self, "WAV 48KHz file fast streamer (big-endian platform)")
+
+        ##################################################
+        # Parameters
+        ##################################################
+        self.input_file = input_file
+        self.port = port
+        self.destination = destination
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.blocks_wavfile_source_0 = blocks.wavfile_source(input_file, False)
+        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_short*1, destination, port, 1472, True)
+        self.blocks_float_to_short_0 = blocks.float_to_short(1, 32767)
+        self.blocks_endian_swap_0 = blocks.endian_swap(2)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_endian_swap_0, 0), (self.blocks_udp_sink_0, 0))    
+        self.connect((self.blocks_float_to_short_0, 0), (self.blocks_endian_swap_0, 0))    
+        self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_float_to_short_0, 0))    
+
+
+    def get_input_file(self):
+        return self.input_file
+
+    def set_input_file(self, input_file):
+        self.input_file = input_file
+
+    def get_port(self):
+        return self.port
+
+    def set_port(self, port):
+        self.port = port
+
+    def get_destination(self):
+        return self.destination
+
+    def set_destination(self, destination):
+        self.destination = destination
+
+
+if __name__ == '__main__':
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    parser.add_option("-f", "--input-file", dest="input_file", type="string", default="",
+        help="Set file [default=%default]")
+    parser.add_option("", "--port", dest="port", type="intx", default=7355,
+        help="Set port [default=%default]")
+    parser.add_option("", "--destination", dest="destination", type="string", default="localhost",
+        help="Set localhost [default=%default]")
+    (options, args) = parser.parse_args()
+    tb = wav_48kHz_fast_be(input_file=options.input_file, port=options.port, destination=options.destination)
+    tb.start()
+    tb.wait()
